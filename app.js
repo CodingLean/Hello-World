99a560ea6d70ff4c537ca621ddaa796d65ab5f5b
Isaiah Mitchell <Isaiah2417@yahoo.com>
2022-09-01 17:43:58 -0500

    added 2 prompts

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 
+
+
+
+
+
+
+
 console.log('Hello World')
 
 let usersName = prompt('Hello, whats your name?');
@@ -9,5 +16,32 @@ if (usersName==='Isaiah'){
 } else{
     Message = 'Hey, Welcome to Kiki and Santanas Page!';
 }
+let kidsName = prompt("Do you know Santanas nickname?")
+let question; 
+
+if(kidsName==='Sosa Baby'){
+    question='SOSA BABY!'
+}
+else {
+    question ="Nah"
+}
+
+ kidsName = prompt("Do you know Ki'ella's nickname?")
+let inquiry; 
+
+if(kidsName==='Princess Kiki'){
+    inquiry='Princess Kiki!'
+}
+else {
+    inquiry ="Nah"
+}
+
+
+
+
+
+
 console.log(Message)
-document.write(Message)
+document.write(Message) 
+document.write(question)
+document.write(inquiry)
