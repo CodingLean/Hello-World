41461251c6698826860d442b7e26cf5dfe4af9c5
Isaiah Mitchell <Isaiah2417@yahoo.com>
2022-09-01 17:20:39 -0500

    Saved Changes

---
+++ app.js
@@ -0,0 +1,13 @@
+
+console.log('Hello World')
+
+let usersName = prompt('Hello, whats your name?');
+let Message;
+
+if (usersName==='Isaiah'){
+    Message = 'Hey Dad!';
+} else
+    Message = 'Hey, Welcome to Kiki and Santanas Page!';
+}
+console.log(Message)
+document.write(Message)
